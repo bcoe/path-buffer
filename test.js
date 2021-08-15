5cfb916c0a85cfa20230c92e02e18bc3502762b4
Benjamin E. Coe <bencoe@google.com>
2021-08-15 19:33:16 -0400

    feat(unicode): support both utf8 and utf16le (#4)

--- test.js
+++ test.js
@@ -4,163 +4,163 @@ const pathString= require('path');
 
 const platformIsWin32 = (process.platform === 'win32');
 
-// dirname should handle absolute path with multiple components.
+// Test dirname()
+
 {
-  const p = [path.sep, 'foo', 'bar', 'apple'].join(path.sep);
-  let buf = Buffer.from(p);
-  assert.strictEqual(path.dirname(buf).toString(), pathString.dirname(p));
+  const dirnameOriginal = path.posix.dirname;
+  for (const encoding of ['utf8', 'utf16le']) {
+    path.posix.dirname = (path) => {
+      return dirnameOriginal(Buffer.from(path, encoding)).toString(path ? encoding : undefined);
+    }
+    assert.strictEqual(path.posix.dirname('/a/b/'), '/a');
+    assert.strictEqual(path.posix.dirname('/a/b'), '/a');
+    assert.strictEqual(path.posix.dirname('/a'), '/');
+    assert.strictEqual(path.posix.dirname(''), '.');
+    assert.strictEqual(path.posix.dirname('/'), '/');
+    assert.strictEqual(path.posix.dirname('////'), '/');
+    assert.strictEqual(path.posix.dirname('//a'), '//');
+    assert.strictEqual(path.posix.dirname('foo'), '.');
+  }
 }
 
-// dirname should handle relative path with multiple components.
-{
-  const p = ['.', 'bar', 'apple'].join(path.sep);
-  const buf = Buffer.from(p);
-  assert.strictEqual(path.dirname(buf).toString(), pathString.dirname(p));
-}
+// Test isAbsolute().
 
-// dirname should handle empty path.
 {
-  const p = '';
-  const buf = Buffer.from(p);
-  assert.strictEqual(path.dirname(buf).toString(), pathString.dirname(p));
-}
-
-// dirname retains // at beginning of path.
-if (!platformIsWin32) {
-  const p = '//foo';
-  const buf = Buffer.from(p);
-  assert.strictEqual(path.dirname(buf).toString(), '//');
-  assert.strictEqual(path.dirname(path.dirname(buf)).toString(), '/');
-}
-
-// isAbsolute returns false if path is relative.
-{
-  const p = '.';
-  const buf = Buffer.from(p);
-  assert.strictEqual(path.isAbsolute(buf), false);
-}
-
-// isAbsolute returns true if path is absolute.
-{
-  const p = [path.sep, 'foo', 'bar', 'apple'].join(path.sep);
-  const buf = Buffer.from(p);
-  assert.strictEqual(path.isAbsolute(buf), true);
+  const isAbsoluteOriginal = path.posix.isAbsolute;
+  for (const encoding of ['utf8', 'utf16le']) {
+    path.posix.isAbsolute = (path) => {
+      return isAbsoluteOriginal(Buffer.from(path, encoding));
+    }
+    assert.strictEqual(path.posix.isAbsolute('/home/foo'), true);
+    assert.strictEqual(path.posix.isAbsolute('/home/foo/..'), true);
+    assert.strictEqual(path.posix.isAbsolute('bar/'), false);
+    assert.strictEqual(path.posix.isAbsolute('./baz'), false);
+  }
 }
 
 // Test join().
 
-// join allows set of relative paths to be joined.
-const failures = [];
-const joinTests = [
-  [ [path.posix.join],
-    // Arguments                     result
-    [[['.', 'x/b', '..', '/b/c.js'], 'x/b/c.js'],
-     [[], '.'],
-     [['/.', 'x/b', '..', '/b/c.js'], '/x/b/c.js'],
-     [['/foo', '../../../bar'], '/bar'],
-     [['foo', '../../../bar'], '../../bar'],
-     [['foo/', '../../../bar'], '../../bar'],
-     [['foo/x', '../../../bar'], '../bar'],
-     [['foo/x', './bar'], 'foo/x/bar'],
-     [['foo/x/', './bar'], 'foo/x/bar'],
-     [['foo/x/', '.', 'bar'], 'foo/x/bar'],
-     [['./'], './'],
-     [['.', './'], './'],
-     [['.', '.', '.'], '.'],
-     [['.', './', '.'], '.'],
-     [['.', '/./', '.'], '.'],
-     [['.', '/////./', '.'], '.'],
-     [['.'], '.'],
-     [['', '.'], '.'],
-     [['', 'foo'], 'foo'],
-     [['foo', '/bar'], 'foo/bar'],
-     [['', '/foo'], '/foo'],
-     [['', '', '/foo'], '/foo'],
-     [['', '', 'foo'], 'foo'],
-     [['foo', ''], 'foo'],
-     [['foo/', ''], 'foo/'],
-     [['foo', '', '/bar'], 'foo/bar'],
-     [['./', '..', '/foo'], '../foo'],
-     [['./', '..', '..', '/foo'], '../../foo'],
-     [['.', '..', '..', '/foo'], '../../foo'],
-     [['', '..', '..', '/foo'], '../../foo'],
-     [['/'], '/'],
-     [['/', '.'], '/'],
-     [['/', '..'], '/'],
-     [['/', '..', '..'], '/'],
-     [[''], '.'],
-     [['', ''], '.'],
-     [[' /foo'], ' /foo'],
-     [[' ', 'foo'], ' /foo'],
-     [[' ', '.'], ' '],
-     [[' ', '/'], ' /'],
-     [[' ', ''], ' '],
-     [['/', 'foo'], '/foo'],
-     [['/', '/foo'], '/foo'],
-     [['/', '//foo'], '/foo'],
-     [['/', '', '/foo'], '/foo'],
-     [['', '/', 'foo'], '/foo'],
-     [['', '/', '/foo'], '/foo'],
+{
+  // join allows set of relative paths to be joined.
+  const failures = [];
+  const joinTests = [
+    [ [path.posix.join],
+      // Arguments                     result
+      [[['.', 'x/b', '..', '/b/c.js'], 'x/b/c.js'],
+      [[], '.'],
+      [['/.', 'x/b', '..', '/b/c.js'], '/x/b/c.js'],
+      [['/foo', '../../../bar'], '/bar'],
+      [['foo', '../../../bar'], '../../bar'],
+      [['foo/', '../../../bar'], '../../bar'],
+      [['foo/x', '../../../bar'], '../bar'],
+      [['foo/x', './bar'], 'foo/x/bar'],
+      [['foo/x/', './bar'], 'foo/x/bar'],
+      [['foo/x/', '.', 'bar'], 'foo/x/bar'],
+      [['./'], './'],
+      [['.', './'], './'],
+      [['.', '.', '.'], '.'],
+      [['.', './', '.'], '.'],
+      [['.', '/./', '.'], '.'],
+      [['.', '/////./', '.'], '.'],
+      [['.'], '.'],
+      [['', '.'], '.'],
+      [['', 'foo'], 'foo'],
+      [['foo', '/bar'], 'foo/bar'],
+      [['', '/foo'], '/foo'],
+      [['', '', '/foo'], '/foo'],
+      [['', '', 'foo'], 'foo'],
+      [['foo', ''], 'foo'],
+      [['foo/', ''], 'foo/'],
+      [['foo', '', '/bar'], 'foo/bar'],
+      [['./', '..', '/foo'], '../foo'],
+      [['./', '..', '..', '/foo'], '../../foo'],
+      [['.', '..', '..', '/foo'], '../../foo'],
+      [['', '..', '..', '/foo'], '../../foo'],
+      [['/'], '/'],
+      [['/', '.'], '/'],
+      [['/', '..'], '/'],
+      [['/', '..', '..'], '/'],
+      [[''], '.'],
+      [['', ''], '.'],
+      [[' /foo'], ' /foo'],
+      [[' ', 'foo'], ' /foo'],
+      [[' ', '.'], ' '],
+      [[' ', '/'], ' /'],
+      [[' ', ''], ' '],
+      [['/', 'foo'], '/foo'],
+      [['/', '/foo'], '/foo'],
+      [['/', '//foo'], '/foo'],
+      [['/', '', '/foo'], '/foo'],
+      [['', '/', 'foo'], '/foo'],
+      [['', '/', '/foo'], '/foo'],
+      ],
     ],
-  ],
-];
-joinTests.forEach((test) => {
-  if (!Array.isArray(test[0]))
-    test[0] = [test[0]];
-  test[0].forEach((join) => {
-    test[1].forEach((test) => {
-      const t = test[0].map((p) => Buffer.from(p));
-      const actual = join.apply(null, t).toString();
-      const expected = test[1];
-      // For non-Windows specific tests with the Windows join(), we need to try
-      // replacing the slashes since the non-Windows specific tests' `expected`
-      // use forward slashes
-      const os = 'posix';
-      if (actual !== expected) {
-        const delimiter = test[0].map(JSON.stringify).join(',');
-        const message = `path.${os}.join(${delimiter})\n  expect=${
-          JSON.stringify(expected)}\n  actual=${JSON.stringify(actual)}`;
-        failures.push(`\n${message}`);
-      }
-    });
+  ];
+  joinTests.forEach((test) => {
+    if (!Array.isArray(test[0]))
+      test[0] = [test[0]];
+    for (const encoding of ['utf8', 'utf16le']) {
+      test[0].forEach((join) => {
+        test[1].forEach((test) => {
+          const hasAnyPaths = test[0].join('') !== '';
+          const t = test[0].map((p) => Buffer.from(p, encoding));
+          const actual = join.apply(null, t).toString(hasAnyPaths ? encoding : undefined);
+          const expected = test[1];
+          // For non-Windows specific tests with the Windows join(), we need to try
+          // replacing the slashes since the non-Windows specific tests' `expected`
+          // use forward slashes
+          const os = 'posix';
+          if (actual !== expected) {
+            const delimiter = test[0].map(JSON.stringify).join(',');
+            const message = `path.${os}.join(${delimiter})\n  expect=${
+              JSON.stringify(expected)}\n  actual=${JSON.stringify(actual)}`;
+            failures.push(`\n${message}`);
+            throw Error(message);
+          }
+        });
+      });
+    }
   });
-});
-assert.strictEqual(failures.length, 0, failures.join(''));
+  assert.strictEqual(failures.length, 0, failures.join(''));
+}
 
 // Test normalize().
 
-const normalizeOriginal = path.posix.normalize;
-path.posix.normalize = (path) => {
-  const p = normalizeOriginal(Buffer.from(path));
-  return p.toString();
+{
+  const normalizeOriginal = path.posix.normalize;
+  for (const encoding of ['utf8', 'utf16le']) {
+    path.posix.normalize = (path) => {
+      const p = normalizeOriginal(Buffer.from(path, encoding));
+      return p.toString(encoding);
+    }
+    assert.strictEqual(path.posix.normalize('./fixtures///b/../b/c.js'),
+                      'fixtures/b/c.js');
+    assert.strictEqual(path.posix.normalize('/foo/../../../bar'), '/bar');
+    assert.strictEqual(path.posix.normalize('a//b//../b'), 'a/b');
+    assert.strictEqual(path.posix.normalize('a//b//./c'), 'a/b/c');
+    assert.strictEqual(path.posix.normalize('a//b//.'), 'a/b');
+    assert.strictEqual(path.posix.normalize('/a/b/c/../../../x/y/z'), '/x/y/z');
+    assert.strictEqual(path.posix.normalize('///..//./foo/.//bar'), '/foo/bar');
+    assert.strictEqual(path.posix.normalize('bar/foo../../'), 'bar/');
+    assert.strictEqual(path.posix.normalize('bar/foo../..'), 'bar');
+    assert.strictEqual(path.posix.normalize('bar/foo../../baz'), 'bar/baz');
+    assert.strictEqual(path.posix.normalize('bar/foo../'), 'bar/foo../');
+    assert.strictEqual(path.posix.normalize('bar/foo..'), 'bar/foo..');
+    assert.strictEqual(path.posix.normalize('../foo../../../bar'), '../../bar');
+    assert.strictEqual(path.posix.normalize('../.../.././.../../../bar'),
+                      '../../bar');
+    assert.strictEqual(path.posix.normalize('../../../foo/../../../bar'),
+                      '../../../../../bar');
+    assert.strictEqual(path.posix.normalize('../../../foo/../../../bar/../../'),
+                      '../../../../../../');
+    assert.strictEqual(
+      path.posix.normalize('../foobar/barfoo/foo/../../../bar/../../'),
+      '../../'
+    );
+    assert.strictEqual(
+      path.posix.normalize('../.../../foobar/../../../bar/../../baz'),
+      '../../../../baz'
+    );
+    assert.strictEqual(path.posix.normalize('foo/bar\\baz'), 'foo/bar\\baz');
+  }
 }
-
-assert.strictEqual(path.posix.normalize('./fixtures///b/../b/c.js'),
-                   'fixtures/b/c.js');
-assert.strictEqual(path.posix.normalize('/foo/../../../bar'), '/bar');
-assert.strictEqual(path.posix.normalize('a//b//../b'), 'a/b');
-assert.strictEqual(path.posix.normalize('a//b//./c'), 'a/b/c');
-assert.strictEqual(path.posix.normalize('a//b//.'), 'a/b');
-assert.strictEqual(path.posix.normalize('/a/b/c/../../../x/y/z'), '/x/y/z');
-assert.strictEqual(path.posix.normalize('///..//./foo/.//bar'), '/foo/bar');
-assert.strictEqual(path.posix.normalize('bar/foo../../'), 'bar/');
-assert.strictEqual(path.posix.normalize('bar/foo../..'), 'bar');
-assert.strictEqual(path.posix.normalize('bar/foo../../baz'), 'bar/baz');
-assert.strictEqual(path.posix.normalize('bar/foo../'), 'bar/foo../');
-assert.strictEqual(path.posix.normalize('bar/foo..'), 'bar/foo..');
-assert.strictEqual(path.posix.normalize('../foo../../../bar'), '../../bar');
-assert.strictEqual(path.posix.normalize('../.../.././.../../../bar'),
-                   '../../bar');
-assert.strictEqual(path.posix.normalize('../../../foo/../../../bar'),
-                   '../../../../../bar');
-assert.strictEqual(path.posix.normalize('../../../foo/../../../bar/../../'),
-                   '../../../../../../');
-assert.strictEqual(
-  path.posix.normalize('../foobar/barfoo/foo/../../../bar/../../'),
-  '../../'
-);
-assert.strictEqual(
-  path.posix.normalize('../.../../foobar/../../../bar/../../baz'),
-  '../../../../baz'
-);
-assert.strictEqual(path.posix.normalize('foo/bar\\baz'), 'foo/bar\\baz');
